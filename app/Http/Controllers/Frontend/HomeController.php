416241b375aa2794f083388a65a08932fbf45788
usama ahmed <uusa35@gmail.com>
2017-08-08 11:20:25 +0300

    test

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -49,6 +49,7 @@ class HomeController extends Controller
         var_dump('4');
         $sliders = Slider::orderBy('order', 'desc')->get();
         var_dump('5');
+        return 'stop';
         return view('frontend.home', compact('sliders', 'commercials', 'mostVisitedAds', 'commercialsFixed', 'commercialsNotFixed','latestAds'));
     }
 
