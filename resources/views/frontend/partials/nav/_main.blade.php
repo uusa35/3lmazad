4cf1ddee88d909282acf67c679c436b392f9033c
usama ahmed <uusa35@gmail.com>
2017-08-08 16:07:12 +0300

    doing some fixing to pagination + return the global scope again

--- resources/views/frontend/partials/nav/_main.blade.php
+++ resources/views/frontend/partials/nav/_main.blade.php
@@ -1,17 +1,19 @@
 <nav class="navbar navbar-wd" id="navbar">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" id="slide-nav"><span class="icon-bar"></span> <span
-                        class="icon-bar"></span> <span class="icon-bar"></span></button>
-            <!--  Logo  --> <a class="logo" href="{{ route('home') }}">
-                <img class="logo logo-default img-responsive"
-                     src="{{ asset('storage/uploads/images/medium/'.$contactus->logo) }}" alt=""/>
-                <img class="logo logo-mobile img-responsive"
-                     src="{{ asset('storage/uploads/images/medium/'.$contactus->logo) }}" alt=""/>
-                <img class="logo logo-transparent img-responsive"
-                     src="{{ asset('storage/uploads/images/medium/'.$contactus->logo) }}" alt=""/>
-            </a>
-            <!-- End Logo -->
+            @if(isset($contactus))
+                <button type="button" class="navbar-toggle" id="slide-nav"><span class="icon-bar"></span> <span
+                            class="icon-bar"></span> <span class="icon-bar"></span></button>
+                <!--  Logo  --> <a class="logo" href="{{ route('home') }}">
+                    <img class="logo logo-default img-responsive"
+                         src="{{ asset('storage/uploads/images/medium/'.$contactus->logo) }}" alt=""/>
+                    <img class="logo logo-mobile img-responsive"
+                         src="{{ asset('storage/uploads/images/medium/'.$contactus->logo) }}" alt=""/>
+                    <img class="logo logo-transparent img-responsive"
+                         src="{{ asset('storage/uploads/images/medium/'.$contactus->logo) }}" alt=""/>
+                </a>
+                @endif
+                        <!-- End Logo -->
         </div>
         {{--SEARCH FORM--}}
         <div class="pull-left search-focus-fade" id="slidemenu" style="display: flex; justify-content: flex-end;">
