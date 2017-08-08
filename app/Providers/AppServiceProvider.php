582d9b4bf527e5ae6c3e0c4caca55ff8f76d91d5
usama ahmed <uusa35@gmail.com>
2017-08-08 16:13:55 +0300

    doing some fixing to pagination + return the global scope again

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,12 +40,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local', 'seeding', 'testing','production')) {
+        if ($this->app->environment('local', 'seeding', 'testing')) {
             $this->app->register(IdeHelperServiceProvider::class);
             $this->app->register(GeneratorsServiceProvider::class);
             $this->app->register(ArtisanViewServiceProvider::class);
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
             $this->app->register(DuskServiceProvider::class);
+        } elseif($this->app()->environment('production')) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
         }
     }
 }
