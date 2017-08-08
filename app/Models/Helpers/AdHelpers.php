c584983ec56459ece9e931bde18e9dc2e08cb9d8
usama ahmed <uusa35@gmail.com>
2017-08-08 12:05:43 +0300

    production

--- app/Models/Helpers/AdHelpers.php
+++ app/Models/Helpers/AdHelpers.php
@@ -20,17 +20,17 @@ trait AdHelpers
     public function getMostVisitedAds($take = 10)
     {
         return $this->selectRaw('ads.*, count(*) as ad_count')
-            ->join('favorites', 'ads.id', '=', 'favorites.ad_id')
+            ->join('ad_visitors', 'ads.id', '=', 'ad_visitors.ad_id')
             ->groupBy('ad_id')// responsible to get the sum of ads returned
             ->orderBy('ad_count', 'DESC')
 //            ->whereHas('user.roles', function ($q) {
 //                return $q;
 //            })
-            ->whereHas('deals', function ($q) {
-                return $q->whereHas('plan', function ($q) {
-                    return $q;
-                });
-            })
+//            ->whereHas('deals', function ($q) {
+//                return $q->whereHas('plan', function ($q) {
+//                    return $q;
+//                });
+//            })
             ->take($take)->get();
     }
 
