a53fe8dcbc0cf3cd64ea81c3383ddb0aae7c6389
usama ahmed <uusa35@gmail.com>
2017-08-13 18:38:22 +0300

    fixing some bugs

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -4,32 +4,28 @@
 <body>
 @include('frontend.partials._loading')
 @include('frontend.partials._modal_search')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="wrapper">
-                @section('header')
-                    @include('frontend.partials.header')
-                @show
-                <div id="pageContent" class="page-content">
-                    @include('frontend.partials.notifications')
-                    @section('breadcrumbs')
-                    @show
-                    @section('content')
-                    @show
-                </div>
-                {{--@include('frontend.partials.components.modals._quick-view')--}}
-
-                @section('footer')
-                    <div class="divider divider--lg"></div>
-                    <div class="hidden" id="lang">{{ app()->getLocale() }}</div>
-                    @include('frontend.partials.footer')
-                @show
-                @section('scripts')
-                    @include('frontend.partials.scripts')
-                @show
-            </div>
+<div class="container">
+    <div class="wrapper">
+        @section('header')
+            @include('frontend.partials.header')
+        @show
+        <div id="pageContent" class="page-content">
+            @include('frontend.partials.notifications')
+            @section('breadcrumbs')
+            @show
+            @section('content')
+            @show
         </div>
+        {{--@include('frontend.partials.components.modals._quick-view')--}}
+
+        @section('footer')
+            <div class="divider divider--lg"></div>
+            <div class="hidden" id="lang">{{ app()->getLocale() }}</div>
+            @include('frontend.partials.footer')
+        @show
+        @section('scripts')
+            @include('frontend.partials.scripts')
+        @show
     </div>
 </div>
 </body>
