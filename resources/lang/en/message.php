9514b10ef9343e8dd5e2fb412144afe6e7b82ad6
usama ahmed <uusa35@gmail.com>
2017-08-14 02:01:47 -0400

    tranlsations

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -1,11 +1,12 @@
 <?php
 
 return array (
+  'choose_main_category' => 'Choose Main Category',
   'max_price_field' => 'Highest Price',
   'category_field' => 'Please Choose Category Field.',
   'area_field' => 'Select Area',
   'min_price_field' => 'Lowest Price',
-  'choose_main_category' => 'Choose Main Category',
   'choose_sub_category' => 'Choose Sub Category',
+  'merchants_message_page' => 'Merchants , Companies Grouped according to different Categories & fields. kindly visit our page contact us for more information.',
   'brand_id_hints' => 'Brands You want to search about.',
 );
