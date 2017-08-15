f997f7810f487bcd1186909a104bd5f48d39e7da
usama ahmed <uusa35@gmail.com>
2017-08-15 15:58:24 +0300

    adjust final issues

--- resources/views/frontend/partials/components/ad-show/_ad-gallery.blade.php
+++ resources/views/frontend/partials/components/ad-show/_ad-gallery.blade.php
@@ -1,7 +1,7 @@
 <div class="product-main-image">
     <div class="product-main-image__item">
         <img class="product-zoom" src='{{ asset('storage/uploads/images/thumbnail/'.$element->image) }}'
-             data-zoom-image="{{ asset('storage/uploads/images/thumbnail/'.$element->image) }}"
+             data-zoom-image="{{ asset('storage/uploads/images/large/'.$element->image) }}"
         />
     </div>
     <div class="product-main-image__zoom ad-click-gallery"></div>
@@ -10,7 +10,7 @@
     <div class="product-images-carousel">
         <ul id="smallGallery">
             <li>
-                <a href="#" data-image="{{ asset('storage/uploads/images/thumbnail/'.$element->image) }}"
+                <a href="#" data-image="{{ asset('storage/uploads/images/large/'.$element->image) }}"
                    data-zoom-image="{{ asset('storage/uploads/images/large/'.$element->image) }}"
                 >
                     <img src="{{ asset('storage/uploads/images/thumbnail/'.$element->image) }}"
@@ -19,7 +19,7 @@
             </li>
             @foreach($element->gallery->first()->images as $image)
                 <li>
-                    <a href="#" data-image="{{ asset('storage/uploads/images/thumbnail/'.$image->image) }}"
+                    <a href="#" data-image="{{ asset('storage/uploads/images/large/'.$image->image) }}"
                        data-zoom-image="{{ asset('storage/uploads/images/large/'.$image->image) }}"
                     >
                         <img src="{{ asset('storage/uploads/images/thumbnail/'.$image->image) }}"
