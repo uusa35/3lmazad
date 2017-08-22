80f22e13a1ce42058ad36673e143b37b8a11c6cb
usama ahmed <uusa35@gmail.com>
2017-08-22 10:34:52 +0300

    doing finish parts

--- resources/views/frontend/partials/components/ad-show/_ad-tabs.blade.php
+++ resources/views/frontend/partials/components/ad-show/_ad-tabs.blade.php
@@ -2,8 +2,8 @@
     <div class="container">
         <!-- Nav tabs -->
         <ul class="nav nav-tabs nav-tabs--wd" role="tablist">
-            <li class="active"><a href="#auctions" aria-controls="home" role="tab" data-toggle="tab"
-                                  class="text-uppercase">{{ trans('general.auctions') }}</a></li>
+            {{--<li class="active"><a href="#auctions" aria-controls="home" role="tab" data-toggle="tab"--}}
+                                  {{--class="text-uppercase">{{ trans('general.auctions') }}</a></li>--}}
             <li><a href="#comments" role="tab" data-toggle="tab"
                    class="text-uppercase">{{ trans('general.comments') }}</a></li>
             {{--<li><a href="#Tab3" role="tab" data-toggle="tab" class="text-uppercase">Tags</a></li>--}}
@@ -13,44 +13,44 @@
 
         <!-- Tab panes -->
         <div class="tab-content tab-content--wd">
-            <div role="tabpanel" class="tab-pane active" id="auctions">
+            {{--<div role="tabpanel" class="tab-pane active" id="auctions">--}}
+            {{--<div class="ui comments">--}}
+            {{--<h3 class="ui dividing header">{{ trans('general.auctions') }}</h3>--}}
+            {{--@foreach($element->auctions as $auction)--}}
+            {{--<div class="comment">--}}
+            {{--<a class="avatar">--}}
+            {{--<img src="{{ asset('storage/uploads/images/thumbnail/'.$auction->user->avatar) }}">--}}
+            {{--</a>--}}
+            {{--<div class="content">--}}
+            {{--<a class="author">{{ $auction->user->name }}</a>--}}
+            {{--<div class="metadata">--}}
+            {{--<span class="date">{{ $auction->fromDate }}</span>--}}
+            {{--</div>--}}
+            {{--<div class="text">--}}
+            {{--{{ $auction->amount }}--}}
+            {{--</div>--}}
+            {{--</div>--}}
+            {{--</div>--}}
+            {{--@endforeach--}}
+            {{--@if(auth()->check())--}}
+            {{--<form class="ui reply form disabled" method="post" action="{{ route('auction.store') }}">--}}
+            {{--{{ csrf_field() }}--}}
+            {{--<div class="field">--}}
+            {{--<textarea name="amount"></textarea>--}}
+            {{--</div>--}}
+            {{--<input type="hidden" name="ad_id" value="{{ $element->id }}">--}}
+            {{--<input type="hidden" name="user_id" value="{{ auth()->user()->id }}">--}}
+            {{--<button type="submit"--}}
+            {{--class="ui blue labeled submit icon button col-lg-{{ app()->isLocale('en') ? 'push' : 'pull'}}-10">--}}
+            {{--<i class="icon edit"></i> {{ trans('general.add_auction') }}--}}
+            {{--</button>--}}
+            {{--</form>--}}
+            {{--@endif--}}
+            {{--</div>--}}
+            {{--</div>--}}
+            <div role="tabpanel" class="tab-pane active" id="comments">
                 <div class="ui comments">
-                    <h3 class="ui dividing header">{{ trans('general.auctions') }}</h3>
-                    @foreach($element->auctions as $auction)
-                        <div class="comment">
-                            <a class="avatar">
-                                <img src="{{ asset('storage/uploads/images/thumbnail/'.$auction->user->avatar) }}">
-                            </a>
-                            <div class="content">
-                                <a class="author">{{ $auction->user->name }}</a>
-                                <div class="metadata">
-                                    <span class="date">{{ $auction->fromDate }}</span>
-                                </div>
-                                <div class="text">
-                                    {{ $auction->amount }}
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                    @if(auth()->check())
-                        <form class="ui reply form disabled" method="post" action="{{ route('auction.store') }}">
-                            {{ csrf_field() }}
-                            <div class="field">
-                                <textarea name="amount"></textarea>
-                            </div>
-                            <input type="hidden" name="ad_id" value="{{ $element->id }}">
-                            <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
-                            <button type="submit"
-                                    class="ui blue labeled submit icon button col-lg-{{ app()->isLocale('en') ? 'push' : 'pull'}}-10">
-                                <i class="icon edit"></i> {{ trans('general.add_auction') }}
-                            </button>
-                        </form>
-                    @endif
-                </div>
-            </div>
-            <div role="tabpanel" class="tab-pane" id="comments">
-                <div class="ui comments">
-                    <h3 class="ui dividing header">{{ trans('general.comments') }}</h3>
+                    <h3 class="ui dividing header">{{ trans('general.auctions_comments') }}</h3>
                     @foreach($element->comments as $comment)
                         <div class="comment">
                             <a class="avatar">
