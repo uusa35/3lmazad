9385203dfbd88d3aef9bb3619aa6a90047b69bcd
usama ahmed <uusa35@gmail.com>
2017-09-05 23:41:37 +0200

    fixing tabs in faq page

--- resources/views/frontend/pages/faq.blade.php
+++ resources/views/frontend/pages/faq.blade.php
@@ -9,10 +9,10 @@
                     <div class="panel panel-default" role="tablist">
                         <div class="panel-heading" role="tab">
                             <h4 class="panel-title">
-                                <a role="button" data-toggle="collapse" href="#collapseOne"> {{ $element->title }} </a>
+                                <a role="button" data-toggle="collapse" href="#collapse-{{ $element->id }}"> {{ $element->title }} </a>
                             </h4>
                         </div>
-                        <div id="collapseOne" class="panel-collapse collapse" role="tabpane-{{ $element->id }}">
+                        <div id="collapse-{{ $element->id }}" class="panel-collapse collapse" role="tabpane-{{ $element->id }}">
                             <div class="panel-body">
                                 <p>
                                     {{ $element->body }}
