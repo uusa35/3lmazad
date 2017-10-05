8588d32c714ff014cea28f43ff6e9c34c3dfbb63
usama ahmed <uusa35@gmail.com>
2017-10-05 19:23:28 +0300

    from local

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,21 +3,21 @@
 @section('content')
     <section class="content top-null">
         <div class="container">
-            @if(!$commercialsFixed->isEmpty())
-                <div class="col-lg-3 col-md-12 aside-column text-center commercial">
+            <div class="col-lg-3 col-md-12 aside-column text-center commercial">
+                @if(!$commercialsFixed->isEmpty())
                     @include('frontend.partials.components.home._commercial-ad',['commercials' => $commercialsFixed])
-                </div>
-            @endif
-            @if(!$sliders->isEmpty())
-                <div class="col-lg-6 col-md-12 aside-column text-center">
+                @endif
+            </div>
+            <div class="col-lg-6 col-md-12 aside-column text-center">
+                @if(!$sliders->isEmpty())
                     @include('frontend.partials.components.home._slider-sm')
-                </div>
-            @endif
-            @if(!$commercialsNotFixed->isEmpty())
-                <div class="col-lg-3 col-md-12 aside-column text-center commercial">
+                @endif
+            </div>
+            <div class="col-lg-3 col-md-12 aside-column text-center commercial">
+                @if(!$commercialsNotFixed->isEmpty())
                     @include('frontend.partials.components.home._commercial-ad',['commercials' => $commercialsNotFixed])
-                </div>
-            @endif
+                @endif
+            </div>
         </div>
     </section>
     @include('frontend.partials.components.home._icons_home_page')
