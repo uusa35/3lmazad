64255ce8d38e3fdedfdd4bf6186680ef94d2f56a
usama ahmed <uusa35@gmail.com>
2017-10-05 20:39:24 +0300

    doing the production

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        session()->put('info','يرجى عدم إدخال أي بيانات حقيقية حيث أن الموقع في مرحلة الاختبار ');
         return view('backend.home');
     }
 
--- resources/views/frontend/partials/notifications.blade.php
+++ resources/views/frontend/partials/notifications.blade.php
@@ -1,113 +1,111 @@
 <div class="" style="display: flex; justify-content : center; align-content center; width: 100%; margin-top: 10px;">
-        @section('notification')
-            @if ($message = Session::get('success'))
-                <div class="alert home-alert home-alert home-alert alert-success alert-block" role="alert">
-                    <div class="col-lg-2">
-                        <i class="fa fa-2x fa-check-circle-o fa-fw"></i>
-                    </div>
-                    <div class="col-lg-20">
-                        <button type="button" class="close" data-dismiss="alert"
-                                style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
-                        @if(is_array($message))
-                            @foreach ($message as $m)
-                                {{ $m }}
-                            @endforeach
-                        @else
-                            {{ $message }}
-                        @endif
-                    </div>
+    @section('notification')
+        @if ($message = Session::get('success'))
+            <div class="alert home-alert home-alert home-alert alert-success alert-block" role="alert">
+                <div class="col-lg-2">
+                    <i class="fa fa-2x fa-check-circle-o fa-fw"></i>
                 </div>
-            @endif
-
-            @if ($message = Session::get('error'))
-                <div class="alert home-alert alert-danger alert-block">
-                    <div class="col-lg-2">
-                        <i class="remove big icon"></i>
-                    </div>
-                    <div class="col-lg-10">
-                        <button type="button" class="close" data-dismiss="alert"
-                                style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
-                        @if(is_array($message))
-                            @foreach ($message as $m)
-                                {{ $m }}
-                            @endforeach
-                        @else
-                            {{ $message }}
-                        @endif
-                    </div>
+                <div class="col-lg-20">
+                    <button type="button" class="close" data-dismiss="alert"
+                            style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
+                    @if(is_array($message))
+                        @foreach ($message as $m)
+                            {{ $m }}
+                        @endforeach
+                    @else
+                        {{ $message }}
+                    @endif
                 </div>
-            @endif
-
-            @if ($message = Session::get('errors'))
-                <div class="alert home-alert alert-danger alert-block">
-                    <div class="col-lg-2">
-                        <i class="remove circle big icon"></i>
-                    </div>
-                    <div class="col-lg-10">
-                        <button type="button" class="close" data-dismiss="alert"
-                                style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
-                        @if($message->all())
-                            @foreach ($message->all() as $m)
-                                <li>{{ $m }}  </li>
-                            @endforeach
-                        @endif
-                    </div>
-                </div>
-            @endif
-
-            @if ($message = Session::get('warning'))
-                <div class="alert home-alert alert-warning alert-block">
-                    <div class="col-lg-2">
-                        <i class="remove circle big icon"></i>
-                    </div>
-                    <div class="col-lg-10">
-                        <button type="button" class="close" data-dismiss="alert"
-                                style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
-                        @if(is_array($message))
-                            @foreach ($message as $m)
-                                {{ $m }}
-                            @endforeach
-                        @else
-                            {!! $message !!}
-                        @endif
-                    </div>
-                </div>
-            @endif
-
-            @if ($message = Session::get('info'))
-                <div class="alert home-alert alert-info alert-block">
-                    <div class="col-lg-2">
-                        <i class="remove circle big icon"></i>
-                    </div>
-                    <div class="col-lg-10">
-                        <button type="button" class="close" data-dismiss="alert"
-                                style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
-                        @if(is_array($message))
-                            @foreach ($message as $m)
-                                {{ $m }}
-                            @endforeach
-                        @else
-                            {{ $message }}
-                        @endif
-                    </div>
-                </div>
-                <hr>
-    </div>
-    @endif
-    <div id="pushNotification" class="hidden">
-        <div class=" alert home-alert alert-success alert-block" role="alert">
-
-            <div class="col-lg-2">
-                <i class="remove circle big icon"></i>
             </div>
-            <div class="col-lg-10">
-                <button type="button" class="close" data-dismiss="alert"
-                        style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
-                <div id="message"></div>
-            </div>
+        @endif
 
+        @if ($message = Session::get('error'))
+            <div class="alert home-alert alert-danger alert-block">
+                <div class="col-lg-2">
+                    <i class="remove big icon"></i>
+                </div>
+                <div class="col-lg-10">
+                    <button type="button" class="close" data-dismiss="alert"
+                            style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
+                    @if(is_array($message))
+                        @foreach ($message as $m)
+                            {{ $m }}
+                        @endforeach
+                    @else
+                        {{ $message }}
+                    @endif
+                </div>
+            </div>
+        @endif
+
+        @if ($message = Session::get('errors'))
+            <div class="alert home-alert alert-danger alert-block">
+                <div class="col-lg-2">
+                    <i class="remove circle big icon"></i>
+                </div>
+                <div class="col-lg-10">
+                    <button type="button" class="close" data-dismiss="alert"
+                            style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
+                    @if($message->all())
+                        @foreach ($message->all() as $m)
+                            <li>{{ $m }}  </li>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        @endif
+
+        @if ($message = Session::get('warning'))
+            <div class="alert home-alert alert-warning alert-block">
+                <div class="col-lg-2">
+                    <i class="remove circle big icon"></i>
+                </div>
+                <div class="col-lg-10">
+                    <button type="button" class="close" data-dismiss="alert"
+                            style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
+                    @if(is_array($message))
+                        @foreach ($message as $m)
+                            {{ $m }}
+                        @endforeach
+                    @else
+                        {!! $message !!}
+                    @endif
+                </div>
+            </div>
+        @endif
+
+        @if ($message = Session::get('info'))
+            <div class="alert home-alert alert-info alert-block">
+                <div class="col-lg-2">
+                    <i class="remove circle big icon"></i>
+                </div>
+                <div class="col-lg-10" style="text-we">
+                    <button type="button" class="close" data-dismiss="alert"
+                            style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
+                    @if(is_array($message))
+                        @foreach ($message as $m)
+                            {{ $m }}
+                        @endforeach
+                    @else
+                        {{ $message }}
+                    @endif
+                </div>
+            </div>
+        @endif
+        <div id="pushNotification" class="hidden">
+            <div class=" alert home-alert alert-success alert-block" role="alert">
+
+                <div class="col-lg-2">
+                    <i class="remove circle big icon"></i>
+                </div>
+                <div class="col-lg-10">
+                    <button type="button" class="close" data-dismiss="alert"
+                            style="float : {{ app()->isLocale('ar') ? 'left' : 'right' }}">&times;</button>
+                    <div id="message"></div>
+                </div>
+
+            </div>
         </div>
-    </div>
-    <div class="divider divider--xs"></div>
+        <div class="divider divider--xs"></div>
 </div>
 @show
