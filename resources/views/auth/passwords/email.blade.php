3368497b562350d1124b634cdc57726f169af0ad
usama ahmed <uusa35@gmail.com>
2017-10-14 23:16:55 +0300

    some modification to reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,15 +6,16 @@
 @endsection
 
 
-@section('top')
+@section('content')
     <section class="content content--fill top-null">
         <div class="container">
-            <h2 class="h-pad-sm text-uppercase text-center">{{ trans('general.reset_password') }}</h2>
+            <h2 class="h-pad-sm text-uppercase text-center">{{ trans('general._forget_password') }}</h2>
             <h6 class="text-uppercase text-center">{{ trans('message.reset_password') }}</h6>
             {{--@include('frontend.partials.components._steps-process')--}}
             <div class="divider divider--sm"></div>
             <div class="row">
-                <div class="col-md-8 col-md-offset-2 col-sm-6 col-sm-offset-3">
+                <div class="col-lg-3"></div>
+                <div class="col-md-6">
                     <div class="card card--form">
                         <div class="divider divider--xs"></div>
                         @include('frontend.partials.forms._forget_password')
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -10,12 +10,15 @@
             <h2 class="h-pad-sm text-uppercase text-center">{{ trans('general.reset_password') }}</h2>
             <div class="divider divider--sm"></div>
             <div class="row">
-                <div class="card card--form">
-                    <div class="divider divider--xs"></div>
-                    @include('frontend.partials.forms._reset_password')
-                    <div class="card--form__footer btn--with-icon"><a href="{{ route('home') }}">
-                            <span class="fa fa-fw fa-arrow-circle-left fa-sm"></span>{{ trans('general.back') }}
-                        </a>
+                <div class="col-lg-3"></div>
+                <div class="col-md-6">
+                    <div class="card card--form">
+                        <div class="divider divider--xs"></div>
+                        @include('frontend.partials.forms._reset_password')
+                        <div class="card--form__footer btn--with-icon"><a href="{{ route('home') }}">
+                                <span class="fa fa-fw fa-arrow-circle-left fa-sm"></span>{{ trans('general.back') }}
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
