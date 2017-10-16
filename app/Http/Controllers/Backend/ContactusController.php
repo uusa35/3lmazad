d8d701e60add37b5254cd23ec8dd5eea94b41f0d
usama ahmed <uusa35@gmail.com>
2017-10-16 19:46:27 +0300

    some changes

--- app/Http/Controllers/Backend/ContactusController.php
+++ app/Http/Controllers/Backend/ContactusController.php
@@ -80,7 +80,7 @@ class ContactusController extends Controller
             return redirect()->route('backend.contactus.index')->with('error','contactus error');
         }
 
-        $this->saveMimes($contactus, $request, ['logo'], ['200', '70'], true);
+        $this->saveMimes($contactus, $request, ['logo'], ['200', '70'], false);
 
         return redirect()->route('backend.contactus.index')->with('success','contactus updated');
     }
