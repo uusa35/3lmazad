f3dfc7e9efefa3fc465df45bcd045965be424281
usama ahmed <uusa35@gmail.com>
2017-11-01 08:50:48 +0300

    from my local

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -2,10 +2,10 @@
     <!-- Basic -->
     <meta charset="utf-8">
     <title>{{ trans('general.app_name') }}</title>
-    <meta name="keywords" content="{{ trans('general.app_keywords") }}"/>
+    <meta name="keywords" content="{{ trans('general.app_keywords') }}"/>
     <meta name="description" content="{{ trans('general.app_description') }}">
     <meta name="author" content="{{ trans('general.app_author') }}">
-    <link rel="shortcut icon" href="logo.jpg">
+    <link rel="shortcut icon" href="logo.ico">
     <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Web Fonts  -->
