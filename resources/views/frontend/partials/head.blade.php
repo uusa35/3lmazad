c17974930c1ee62dbb6f58ad8802ca8e4ea19642
usama ahmed <uusa35@gmail.com>
2017-11-01 08:55:49 +0300

    from my local

--- resources/views/frontend/partials/head.blade.php
+++ resources/views/frontend/partials/head.blade.php
@@ -5,9 +5,10 @@
     <meta name="keywords" content="{{ trans('general.app_keywords') }}"/>
     <meta name="description" content="{{ trans('general.app_description') }}">
     <meta name="author" content="{{ trans('general.app_author') }}">
-    <link rel="shortcut icon" href="logo.ico">
+    <link rel="shortcut icon" href="{{ asset('images/logo.ico') }}">
     <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Web Fonts  -->
     @include('frontend.partials.styles')
     <meta name="csrf-token" content="{{ csrf_token() }}">
