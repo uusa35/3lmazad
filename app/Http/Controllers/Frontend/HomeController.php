e3fc4043b947d973d1bb077f79d76b42b4bcb43b
usama ahmed <uusa35@gmail.com>
2017-12-07 12:20:32 +0300

    working ...

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -43,6 +43,7 @@ class HomeController extends Controller
     public function index()
     {
         $mostVisitedAds = $this->ad->getMostVisitedAds();
+        dd($mostVisitedAds);
         $latestAds = $this->ad->has('category')->orderBy('created_at', 'desc')->take(15)->get()->reverse();
         $commercialsFixed = $this->commercial->fixed()->orderBy('created_at', 'desc')->take(2)->get();
         $commercialsNotFixed = $this->commercial->notFixed()->inRandomOrder()->take(2)->get();
